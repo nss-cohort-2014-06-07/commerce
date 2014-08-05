86c3f0e00a8fedae2d490cee14c0c2e1f9604bed
Chyld Medford <chyld.medford@gmail.com>
2014-08-05 10:30:03 -0500

    method override and jade templates

--- app/lib/pipeline.js
+++ app/lib/pipeline.js
@@ -2,6 +2,7 @@
 
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var methodOverride = require('express-method-override');
 var home = require('../controllers/home');
 var items = require('../controllers/items');
 
@@ -9,6 +10,7 @@ module.exports = function(app, express){
   app.use(morgan('dev'));
   app.use(express.static(__dirname + '/../static'));
   app.use(bodyParser.urlencoded({extended:true}));
+  app.use(methodOverride());
 
   app.get('/', home.index);
   app.get('/about', home.about);
@@ -18,7 +20,7 @@ module.exports = function(app, express){
   app.post('/items', items.create);
   app.get('/items', items.index);
   app.get('/items/:id', items.show);
-  app.post('/items/:id/delete', items.destroy);
+  app.delete('/items/:id', items.destroy);
 
   console.log('Pipeline Configured');
 };
